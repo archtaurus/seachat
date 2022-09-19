40a07ad6b56accebf66f765136ca7290494e5788
Zhao Xin <7176466@qq.com>
2022-09-19 15:17:29 +0800

    v1.0.5

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const { version, description } = require('./package.json')
 program
     .version(version)
     .description(description)
-    .option('-h, --host <host>', 'set server\'s host', process.env.HOST || 'localhost')
+    .option('-h, --host <host>', 'set server\'s host', process.env.HOST || '0.0.0.0')
     .option('-p, --port <port>', 'set server\'s port', process.env.PORT || 8000)
     .parse()
 
